5279efd0b331082419f5d85685929de91718f9fb
SShubham kumar <shubhamkrg1819@gmail.com>
2025-03-05 13:55:53 +0530

    return request ui logic update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,27 +2,17 @@ import React, { useEffect } from 'react';
 import { Route, Routes, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import UserManagement from './pages/Dashboard/User.jsx';
-import CreateUser from './components/CreateUser.jsx';
 import Sites from './pages/Site/Site.jsx';
-import CreateSite from './components/CreateSite.jsx';
 import Clients from './pages/CRM/Clients.jsx';
-import CreateClient from './components/CreateClient.jsx';
-import CreateEmployee from './components/CreateEmployee.jsx';
 import Profile from './components/Profile.jsx';
 import Login from './components/Login.jsx';
 import Register from './components/Register.jsx';
 import WorkOrders from './pages/Site/WorkOrder.jsx';
-import CreateWorkOrder from './components/CreateWorkOrder.jsx';
-import CreateWorkDetails from './components/CreateWorkDetail.jsx';
 import ProjectSchedules from './pages/Site/ProjectSchedules.jsx';
-import CreateProjectSchedule from './components/CreateProjectSchedule.jsx';
 import PaymentSchedules from './pages/Site/PaymentSchedule.jsx';
-import CreatePaymentSchedule from './components/CreatePaymentSchedule.jsx';
 import Bills from './pages/Site/Bill.jsx';
-import CreateBill from './components/CreateBill.jsx';
 import CheckList from './pages/Site/CheckList.jsx';
 import Contractors from './pages/Site/Contractors.jsx';
-import CreateContractor from './components/CreateContractor.jsx';
 import Admin from './pages/Dashboard/Admin.jsx';
 import Accountant from './pages/Dashboard/Accountant.jsx';
 import Design_Head from './pages/Dashboard/Design_Head.jsx';
@@ -41,10 +31,7 @@ import Payment_SchedulScreen from './screen/Payment_SchedulScreen.jsx';
 import Project_ScheduleScreen from './screen/Project_ScheduleScreen.jsx';
 import ContractorScreen from './screen/ContractorScreen.jsx';
 import WorkDetails from './pages/Dashboard/WorkDetails.jsx';
-import CreateExtraWork from './components/CreateExtraWork.jsx';
 import ExtraWork from './pages/Site/ExtraWork.jsx';
-import CreateSupplier from './components/CreateSupplier.jsx';
-import CreatePurchaseOrder from './components/CreatePurchaseOrder.jsx';
 import PurchaseOrders from './pages/ERP/PurchaseOrders.jsx';
 import Suppliers from './pages/ERP/Suppliers.jsx';
 import PurchaseOrderScreen from './screen/PurchaseOrderScreen.jsx';
@@ -52,7 +39,6 @@ import Employee from './pages/HR/Employee.jsx';
 import ExtraWorkScreen from './screen/ExtraWorkScreen.jsx';
 import { logout } from './features/auth/authSlice.js';
 import QualitySchedules from './pages/Site/QualitySchedules.jsx';
-import CreateQualitySchedule from './components/CreateQualitySchedule.jsx';
 import './index.css';
 import UserProfile from './components/ProfileCard.jsx';
 import SupplierScreen from './screen/SupplierScreen.jsx';
@@ -60,11 +46,9 @@ import QualityScheduleScreen from './screen/QualityScheduleScreen.jsx';
 import Approval from './pages/Dashboard/Approval.jsx';
 import ResetPasswd from './components/ResetPasswd.jsx';
 import Attendance from './pages/Dashboard/Attendance.jsx';
-import CreateLeave from './components/CreateLeave.jsx';
 import Account_Head from './pages/Dashboard/AccountHead.jsx';
 import ERP from './pages/ERP/ERP.jsx';
 import SiteKharchi from './pages/Dashboard/SiteKharchi.jsx';
-import CreateRecord from './components/CreateRecord.jsx';
 import RecordInventory from './components/RecordInventory.jsx';
 import CRM from './pages/CRM/CRM.jsx';
 import InventoryScreen from './screen/InventoryScreen.jsx';
@@ -74,30 +58,23 @@ import Sales from './pages/ERP/Sales.jsx';
 import Lead from './pages/CRM/Lead.jsx';
 import Project from './pages/CRM/Project.jsx';
 import Stock from './pages/ERP/Stock.jsx';
-import Return from './pages/ERP/Return.jsx';
 import Expenses from './pages/ERP/Expenses.jsx';
 import BalanceSheet from './pages/ERP/BalanceSheet.jsx';
 import SitesDashboard from './pages/Site/SitesDashboard.jsx';
-import ReturnOrders from './pages/Site/ReturnOrders.jsx';
+import ReturnOrders from './pages/ERP/ReturnOrders.jsx';
 import { TbInvoice } from 'react-icons/tb';
 import Invoice from './pages/CRM/Invoice.jsx';
 import AccountManagement from './pages/ERP/AccountManagement.jsx';
 import Order from './pages/ERP/Order.jsx';
-import CreateReturnOrder from './components/CreateReturnOrder.jsx';
 import PurchaseRequest from './pages/Site/PurchaseRequest.jsx';
-import CreatePurchaseRequest from './components/CreatePurchaseRequest.jsx';
-import CreateChecklist from './components/CreateChecklist.jsx';
 import Inventory from './pages/ERP/Inventory.jsx';
 import UserScreen from './screen/UserScreen.jsx';
 import MorePage from './components/MorePage.jsx';
 import Report from './pages/Site/Report.jsx';
 import ReturnOrderScreen from './screen/ReturnOrderScreen.jsx';
-import SalesForm from './pages/ERP/SalesForm.jsx';
 import Receipt_Payment from './pages/ERP/Receipt_Payment.jsx';
-import JournalEntryForm from './pages/ERP/Journal.jsx';
 import Contra from './pages/ERP/Contra.jsx';
 import ERPReport from './pages/ERP/ERPReport.jsx';
-import CreateLead from './components/CreateLead.jsx';
 import Journal from './pages/ERP/Journal.jsx';
 import ReturnRequest from './pages/Site/Return.jsx';
 
@@ -145,101 +122,69 @@ const App = () => {
         <Route path='/account-head' element={<Account_Head />} />
 
         <Route path='/attendance' element={<Attendance />} />
-        <Route path='/create-leave' element={<CreateLeave />} />
-        <Route path='/edit-leave/:id' element={<CreateLeave />} />
 
         <Route path='/hr/employee/dashboard' element={<Employee />} />
-        <Route path='/create-employee' element={<CreateEmployee />} />
 
         <Route path='/user' element={<UserManagement />} />
         <Route path='/user/:id' element={<UserScreen />} />
-        <Route path='/create-user' element={<CreateUser />} />
-        <Route path='/edit-user/:id' element={<CreateUser />} />
 
         <Route path='/sites/contractors' element={<Contractors />} />
-        {/* <Route path='/create-contractors' element={<CreateContractor />} /> */}
         <Route path='/contractor/:id' element={<ContractorScreen />} />
-        {/* <Route path='/edit-contractor/:id' element={<CreateContractor />} /> */}
 
           <Route path='/site/report' element={<SitesDashboard />} />
           {/* <Route path='/site/report' element={<Report />} /> */}
           <Route path='/sites' element={<Sites />} />
           <Route path='/site/:id' element={<SiteScreen />} />
-          {/* <Route path='/create-site' element={<CreateSite />} /> */}
-          {/* <Route path='/edit-site/:id' element={<CreateSite />} /> */}
 
           <Route path='/sites/clients' element={<Clients />} />
           <Route path='/client/:id' element={<ClientScreen />} />
-          {/* <Route path='/create-client' element={<CreateClient />} /> */}
-          {/* <Route path='/edit-client/:id' element={<CreateClient />} /> */}
 
           <Route path='/sites/work-orders' element={<WorkOrders />} />
           <Route path='/work-order/:id' element={<WorkOrderScreen />} />
-          {/* <Route path='/create-work-order' element={<CreateWorkOrder />} /> */}
-          <Route path='/edit-workOrder/:id' element={<CreateWorkOrder />} />
-          <Route path='/edit-workOrder/:id/work/:index' element={<CreateWorkOrder />} />
 
 
           <Route path='/sites/project-schedules' element={<ProjectSchedules />} />
           <Route path='/project-schedule/:id' element={<Project_ScheduleScreen />} />
-          {/* <Route path='/create-project-schedule' element={<CreateProjectSchedule />} /> */}
-          <Route path='/edit-projectSchedule/:id/:index' element={<CreateProjectSchedule />} />
+
 
           <Route path='/sites/quality-schedules' element={<QualitySchedules />} />
           <Route path='/quality-schedule/:id' element={<QualityScheduleScreen />} />
-          {/* <Route path='/create-quality-schedule' element={<CreateQualitySchedule />} /> */}
-          <Route path='/edit-projectSchedule/:id' element={<CreateProjectSchedule />} />
-          <Route path='/edit-qualitySchedule/:id/:index' element={<CreateQualitySchedule />} />
-          <Route path='/edit-qualitySchedule/:id' element={<CreateQualitySchedule />} />
+
 
           <Route path='/sites/payment-schedules' element={<PaymentSchedules />} />
           <Route path='/payment-schedule/:id' element={<Payment_SchedulScreen />} />
-          {/* <Route path='/create-payment-schedule' element={<CreatePaymentSchedule />} /> */}
-          <Route path='/edit-paymentSchedule/:id' element={<CreatePaymentSchedule />} />
-          <Route path='/edit-paymentSchedule/:id/:index' element={<CreatePaymentSchedule />} />
+
 
           <Route path='/sites/bills' element={<Bills />} />
           <Route path='/bill/:id' element={<BillScreen />} />
-          {/* <Route path='/create-bill' element={<CreateBill />} /> */}
-          <Route path='/edit-bill/:id' element={<CreateBill />} />
+
 
           <Route path='/sites/extra-work' element={<ExtraWork />} />
           <Route path='/extra-work/:id' element={<ExtraWorkScreen />} />
-          {/* <Route path='/create-extra-work' element={<CreateExtraWork />} /> */}
-          <Route path='/edit-extra-work/:id' element={<CreateExtraWork />} />
-          <Route path='/edit-extra-work/:id/work/:index' element={<CreateExtraWork />} />
+
 
           <Route path='/sites/checklists' element={<CheckList />} />
-          {/* <Route path='/create-checklist' element={<CreateChecklist />} /> */}
+
 
           <Route path='/sites/return' element={<ReturnRequest />} />
 
         <Route path='/suppliers' element={<Suppliers />} />
-        {/* <Route path='/create-supplier' element={<CreateSupplier />} /> */}
         <Route path='/supplier/:id' element={<SupplierScreen />} />
-        <Route path='/edit-supplier/:id' element={<CreateSupplier />} />
 
         <Route path='/site/purchase-request' element={<PurchaseRequest />} />
         <Route path='/erp/inventory/purchase-request' element={<PurchaseRequest />} />
         <Route path='/erp/purchase-order' element={<PurchaseOrders />} />
         <Route path='/purchase-order/:id' element={<PurchaseOrderScreen />} />
         <Route path='/purchase-request/:id' element={<PurchaseOrderScreen />} />
-        {/* <Route path='/create-purchaseOrder' element={<CreatePurchaseOrder />} /> */}
-        {/* <Route path='/create-purchaseRequest' element={<CreatePurchaseRequest />} /> */}
-        <Route path='/edit-purchaseOrder/:id' element={<CreatePurchaseOrder />} />
-        <Route path='/edit-purchaseOrder/:id/material/:index' element={<CreatePurchaseOrder />} />
+
 
         <Route path='/return-order/:from' element={<ReturnOrders />} />
         <Route path='/return-order/:id' element={<ReturnOrderScreen />} />
-        {/* <Route path='/create-returnOrder' element={<CreateReturnOrder />} /> */}
-        <Route path='/edit-returnOrder/:id' element={<CreateReturnOrder />} />
-        <Route path='/edit-returnOrder/:id/material/:index' element={<CreateReturnOrder />} />
 
         <Route path='/erp' element={<ERP />} />
         <Route path='/erp/account' element={<AccountManagement />} />
         <Route path='/erp/expenses' element={<Expenses />} />
         <Route path='/erp/balance-sheet' element={<BalanceSheet />} />
-        <Route path='/erp/create-record' element={<CreateRecord />} />
         <Route path='/erp/inventory' element={<Store />} />
         <Route path='/erp/report/:of' element={<ERPReport />} />
         <Route path='/erp/inventory/stock' element={<Stock />} />
@@ -270,12 +215,9 @@ const App = () => {
         <Route path='/crm/Invoice' element={<Invoice />} />
 
         <Route path='/site-kharchi' element={<SiteKharchi />} />
-        {/* <Route path='/record-site-kharchi/:userId' element={<CreateRecord />} /> */}
+
 
         <Route path='/work-details' element={<WorkDetails />} />
-        {/* <Route path='/create-work-details' element={<CreateWorkDetails />} /> */}
-        <Route path='/edit-work-detail/:id' element={<CreateWorkDetails />} />
-        <Route path='/edit-work-detail/:id/:index' element={<CreateWorkDetails />} />
 
         <Route path='/setting' element={<Profile />} />
 
--- client/src/pages/Site/Return.jsx
+++ client/src/pages/Site/Return.jsx
@@ -1,168 +1,129 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { NavLink, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
 import toast, { Toaster } from 'react-hot-toast';
-import { GrEdit } from "react-icons/gr";
 import { MdDelete, MdAdd } from "react-icons/md";
-import { FaExternalLinkAlt } from "react-icons/fa";
 import { useSelector } from 'react-redux';
 import Header from '../../components/Header';
 import Modal from '../../components/Modal';
-import ReturnFormModal from '../../components/CreateReturnRequest';
+import ReturnFormModal from '../../components/CreateReturn';
 axios.defaults.withCredentials = true;
 
 const ReturnRequest = () => {
     const navigate = useNavigate();
-    const [ReturnRequest, setReturnRequest] = useState([]);
-    const [draftOrder, setDraftOrder] = useState([]);
+    const [returnRequests, setReturnRequest] = useState([]);  // ✅ Ensure default state is an array
     const [createModal, setCreateModal] = useState(false);
-    const { user, isLoggedIn } = useSelector((state) => state.auth);
-    const [activeTab, setActiveTab] = useState("approved");
+    const { user } = useSelector((state) => state.auth);
 
     useEffect(() => {
         const fetchReturnRequest = async () => {
             try {
-                const ReturnRequestData = await axios.get('/api/v1/return');
-                if (user.department === 'Site Supervisor' || user.department === 'Site Incharge') {
-                    const sites = user?.site;
-                    let ReturnRequest = [];
+                const response = await axios.get('/api/v1/return');
+                console.log("API Response:", response.data); // ✅ Debugging log
+
+                if (!Array.isArray(response.data)) {
+                    throw new Error("Invalid data format: Expected an array");
+                }
+
+                if (user?.department === 'Site Supervisor' || user?.department === 'Site Incharge') {
+                    const sites = user?.site || [];
+                    let filteredRequests = [];
+
                     for (let site of sites) {
-                        const filteredReturnRequest = ReturnRequestData.data?.filter((ReturnRequest) => ReturnRequest.site?._id.includes(site));
-                        ReturnRequest = [...ReturnRequest, ...filteredReturnRequest]
+                        const siteRequests = response.data.filter((req) => req.site?._id.includes(site));
+                        filteredRequests = [...filteredRequests, ...siteRequests];
                     }
-                    setReturnRequest(ReturnRequest)
+
+                    setReturnRequest(filteredRequests);
                 } else {
-                    setReturnRequest(ReturnRequestData.data);
-                }
-                console.log(ReturnRequestData.data)
-            } catch (error) {
-                console.error(error)
-            }
-        };
-        const getDraftOrders = async () => {
-            try {
-                const orderData = await axios.get(`/api/v1/return/draft/${user?._id}`);
-                const orders = orderData.data;
-                if (user.department === 'Site Supervisor' || user.department === 'Site Incharge' && isLoggedIn) {
-                    const sites = user?.site;
-                    // console.log('user', user);
-                    // console.log('sites', sites);
-                    let draftOrders = [];
-                    for (let site of sites) {
-                        const filteredDraftOrders = orders?.filter((order) => order.site?._id.includes(site));
-                        draftOrders = [...draftOrders, ...filteredDraftOrders]
-                    }
-                    setDraftOrder(draftOrders);
-                    console.log(draftOrders);
+                    setReturnRequest(response.data);
                 }
             } catch (error) {
-                toast.error(error.message)
+                console.error("Fetch Error:", error);
+                toast.error("Failed to load return requests.");
+                setReturnRequest([]);  // ✅ Ensure it remains an array
             }
         };
+
         fetchReturnRequest();
-        getDraftOrders();
-    }, [])
-    const handleEdit = (id) => {
-        navigate(`/edit-ReturnRequestOrder/${id}`);
-    };
-    const handleRedirect = (id) => {
-        navigate(`/ReturnRequest-order/${id}`);
-    };
+    }, [user]);
+
     const handleDelete = async (id) => {
         try {
             await axios.delete(`/api/v1/return/${id}`);
-            setReturnRequest(ReturnRequest.filter((ReturnRequest) => ReturnRequest._id !== id));
-            setDraftOrder(draftOrder.filter((order) => order._id !== id));
+            setReturnRequest((prevRequests) => prevRequests.filter((req) => req._id !== id));
         } catch (error) {
-            toast.error(error.message)
+            console.error("Delete Error:", error);
+            toast.error("Failed to delete return request.");
         }
     };
 
-    ReturnRequest(
+    return (
         <div>
             <section className="overflow-x-auto scrollbar-hide">
-                <Header category="Page" title="ReturnRequest Order's" />
-                <div className="w-full mx-auto text-gray-700 p-1 flex flex-row justify-end items-center">
-                    <h2 className="text-lg text-wrap sm:text-md md:text-lg lg:text-xl text-green-600 mr-4 pr-4">
-                        Total ReturnRequest Orders: {ReturnRequest?.length}
+                <Header category="Page" title="Return Request" />
+
+                <div className="w-full mx-auto text-gray-700 p-1 flex flex-row justify-between items-center">
+                    <h2 className="text-lg text-green-600 mr-4 pr-4">
+                        Total Return Requests: {returnRequests.length}
                     </h2>
                     <button onClick={() => setCreateModal(true)} className="bg-green-500 rounded-full text-white px-2 py-2">
                         <MdAdd className='text-xl' />
                     </button>
                 </div>
 
-                {/* <div className="flex space-x-4 border-b-2 mb-4 w-full md:w-auto">
-                    <button
-                        className={`px-4 py-2 ${activeTab === "approved" ? "border-b-4 border-blue-500 font-bold" : "text-gray-500"}`}
-                        onClick={() => setActiveTab("approved")}
-                    >
-                        Approved
-                    </button>
-                    <button
-                        className={`px-4 py-2 ${activeTab === "draft" ? "border-b-4 border-blue-500 font-bold" : "text-gray-500"}`}
-                        onClick={() => setActiveTab("draft")}
-                    >
-                        Drafts
-                    </button>
-                </div> */}
-
-                {/* {activeTab === "approved" && ( */}
-                    <div className="overflow-x-auto scrollbar-hide">
-                        <table className='w-full whitespace-nowrap bg-white divide-y divide-gray-300 overflow-x-auto scrollbar-hide'>
-                            <thead className="bg-gray-300">
-                                <tr className=" text-left">
-                                    <th className="font-semibold text-sm uppercase px-6 py-4 "> Name </th>
-                                    <th className="font-semibold text-sm uppercase px-6 py-4 text-center"> Admin Approve </th>
-                                    {/* <th className="font-semibold text-sm uppercase px-6 py-4 text-center"> Supplier Approve</th> */}
-                                    <th className="font-semibold text-sm uppercase px-6 py-4 text-center"> Total Amount </th>
-                                    <th className="font-semibold text-sm uppercase px-6 py-4 text-center"> Total Paid </th>
-                                    <th className="font-semibold text-sm uppercase px-6 py-4 text-center"> Total Due </th>
-                                    <th className="font-semibold text-sm uppercase px-6 py-4 text-center"></th>
-                                </tr>
-                            </thead>
-                            <tbody className="divide-y divide-gray-200">
-                                {ReturnRequest?.map((ReturnRequest) => (
-                                    <tr key={ReturnRequest._id} className='border-b border-blue-gray-200'>
+                <div className="bg-white rounded-lg shadow overflow-x-auto scrollbar-hide">
+                    <table className="w-full border-collapse overflow-x-auto table-auto whitespace-nowrap">
+                        <thead >
+                            <tr className="text-left bg-gray-300">
+                                <th className="font-semibold text-sm uppercase px-6 py-4"> Name </th>
+                                <th className="font-semibold text-sm uppercase px-6 py-4 text-center"> Admin Approve </th>
+                                <th className="font-semibold text-sm uppercase px-6 py-4 text-center"> Total Amount </th>
+                                <th className="font-semibold text-sm uppercase px-6 py-4 text-center"> Total Paid </th>
+                                <th className="font-semibold text-sm uppercase px-6 py-4 text-center"> Total Due </th>
+                                <th className="font-semibold text-sm uppercase px-6 py-4 text-center"></th>
+                            </tr>
+                        </thead>
+                        <tbody className="divide-y divide-gray-200">
+                            {returnRequests.length > 0 ? (
+                                returnRequests.map((returnRequest) => (
+                                    <tr key={returnRequest._id} className='border-b border-blue-gray-200'>
                                         <td className="px-6 py-4">
-                                            <p className=""> {ReturnRequest.site?.name} </p>
-                                            <p className="text-gray-500 text-sm font-semibold tracking-wide"> {ReturnRequest.supplier?.name} </p>
+                                            <p>{returnRequest.site?.name}</p>
+                                            <p className="text-gray-500 text-sm font-semibold">{returnRequest.supplier?.name}</p>
                                         </td>
-                                        <td className="px-6 py-4 text-center">{ReturnRequest?.adminApprove}</td>
-                                        {/* <td className="px-6 py-4 text-center">{ReturnRequest?.supplierApprove}</td> */}
-                                        <td className="px-6 py-4 text-center">₹ {ReturnRequest?.totalValue ? ReturnRequest?.totalValue : ' 0'}</td>
-                                        <td className="px-6 py-4 text-center">₹ {ReturnRequest?.totalPaid ? ReturnRequest?.totalPaid : ' 0'}</td>
-                                        <td className="px-6 py-4 text-center">₹ {ReturnRequest?.totalDue ? ReturnRequest?.totalDue : ' 0'}</td>
+                                        <td className="px-6 py-4 text-center">{returnRequest?.adminApprove}</td>
+                                        <td className="px-6 py-4 text-center">₹ {returnRequest?.totalValue || 0}</td>
+                                        <td className="px-6 py-4 text-center">₹ {returnRequest?.totalPaid || 0}</td>
+                                        <td className="px-6 py-4 text-center">₹ {returnRequest?.totalDue || 0}</td>
                                         <td className="px-6 py-4 text-center">
-                                            {/* <button onClick={() => handleRedirect(ReturnRequest._id)} className="mr-2">
-                        <FaExternalLinkAlt className='text-blue-500 hover:text-blue-800 text-lg' />
-                      </button>
-                      <button onClick={() => handleEdit(ReturnRequest._id)} className="mr-2">
-                        <GrEdit className="text-green-500 hover:text-green-800 text-lg" />
-                      </button> */}
-                                            <button onClick={() => handleDelete(ReturnRequest._id)} className="mr-2">
+                                            <button onClick={() => handleDelete(returnRequest._id)} className="mr-2">
                                                 <MdDelete className='text-red-500 hover:text-red-600 text-xl' />
                                             </button>
                                         </td>
                                     </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    </div>
-                {/* )} */}
+                                ))
+                            ) : (
+                                <tr>
+                                    <td colSpan="6" className="text-center py-4 text-gray-500">
+                                        No return requests found.
+                                    </td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </table>
+                </div>
 
-
-                <Toaster
-                    position="top-right"
-                    reverseOrder={false} />
+                <Toaster position="top-right" reverseOrder={false} />
             </section>
-            {/* ReturnRequest Order Modal */}
+
             {createModal && (
-                <Modal isOpen={createModal} onClose={() => setCreateModal(false)} head='Create ReturnRequest Order' >
+                <Modal isOpen={createModal} onClose={() => setCreateModal(false)} head='Create Return Request'>
                     <ReturnFormModal onClose={() => setCreateModal(false)} />
                 </Modal>
             )}
         </div>
-    )
-}
+    );
+};
 
-export default ReturnRequest
+export default ReturnRequest;
